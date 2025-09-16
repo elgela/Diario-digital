a5c8c5c9a077ac9de72694870902fd9bed407c5e
Marcelo Gelato <gelatomarcelo77@gmail.com>
2025-09-15 22:06:36 -0300

    php creado

---
+++ nosotros.php
@@ -0,0 +1,17 @@
+<?php
+function showUs() {
+    require_once 'templates/header.php';
+?>
+    <!-- main section -->
+    <main class="container mt-5">
+        <section class="us">
+
+            <h1>Quienes somos</h1>
+            <h2><?php ?></h2>
+
+                <img src="./img/marcelo.jpeg" alt="">
+        </section>
+    </main>
+<?php
+    require_once 'templates/footer.php';
+}
